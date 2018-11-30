2bec7de798b0c5d182ed73c4bdb51b23e4ded520
Mihai Moldovan <mihaimoldovan@Artyoms-MacBook-Pro.local>
2018-11-30 23:16:24 +0200

    First commit

---
+++ Aex/app/src/main/java/aex0733/mld/net/aex0733/MainActivity.java
@@ -0,0 +1,13 @@
+package aex0733.mld.net.aex0733;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
